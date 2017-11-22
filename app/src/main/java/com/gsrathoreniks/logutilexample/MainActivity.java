0692d2bbe5e499c31ec6bae1224eafabb6837e6f
Gajendra Singh Rahtore <niks.gajsa@gmail.com>
2017-11-22 23:47:43 +0530

    Implementation of Library in Example app

--- app/src/main/java/com/gsrathoreniks/logutilexample/MainActivity.java
+++ app/src/main/java/com/gsrathoreniks/logutilexample/MainActivity.java
@@ -3,11 +3,21 @@ package com.gsrathoreniks.logutilexample;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.gsrathoreniks.logutil.*;
+
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LogDebug.d("This is Debug Log");
+        LogVerbose.v("This is Verbose Log");
+        LogError.e("This is Error Log");
+        LogInfo.i("This is Info Log");
+        LogWarn.w("This is Warn Log");
+
     }
 }
